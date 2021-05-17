abd33a35c4df5f54dbaa097d035b3d6164b3ded4
MaGa6969 <jymbo.02@gmail.com>
2021-05-17 15:45:25 +0200

    añadido product

--- src/main/java/org/factoriaf5/store/controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -3,15 +3,22 @@ package org.factoriaf5.store.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class ProductController {
+
     @GetMapping("/health")
     public String check() {
         String greeting = "Hello! I'm a server";
         return greeting;
     }
 
-
+    public List<Product> products = new ArrayList<>();
+    @GetMapping("/products")
+    public List<Product> allProducts(){
+        return products;
+    }
 }
 
 
