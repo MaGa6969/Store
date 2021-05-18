3aff0090f1332f9a8150550aec0bd4ece931e261
MaGa6969 <jymbo.02@gmail.com>
2021-05-18 13:56:20 +0200

    añadiendo postmapping

--- src/main/java/org/factoriaf5/store/controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -2,6 +2,7 @@ package org.factoriaf5.store.controllers;
 
 import org.factoriaf5.store.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -23,6 +24,9 @@ public class ProductController {
         return products;
 
     }
-
+    @PostMapping("/products")
+    public void addNewProduct(Product product) {
+        products.add(product);
+    }
 }
 
